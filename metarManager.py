2ef7026e7917b23836e1e2a83fdffb2f1f6f93b1
Mathew <mathew@mathewjenkinson.com>
2021-03-30 09:41:41 -0400

    Update metarManager.py
    Env Variables
    Periodic push to PubNub with current METARS (this is mostly a feedback loop to know when the last METAR request has been made)

--- metarManager.py
+++ metarManager.py
@@ -1,16 +1,16 @@
+#!/usr/bin/env python3
+
 # Live Sectional Metar Map Controller
 # Mathew Jenkinson March 2021
 
-# Inspiration from and Attribution to Dylan Rush of dylanhrush.com
-# This project uses avwx.rest rather than directly getting the weather from AviationWeather.gov 
-# To use this script you will need an account from avwx.rest which will give you an API key
-
+import os
 import urllib.request
 import simplejson as json
 import time
 import RPi.GPIO as GPIO
 
 apiKey = ""
+#apiKey = os.getenv('avwxKey')
 flightRulesObject = {}
 
 def getMetar(airportCode):
@@ -38,7 +38,14 @@ def getMetar(airportCode):
         # flightRulesObject[airportCode] = jsonMetar['flight_rules']
 
         if len(flightRulesObject) == len(ledPins):
-            # print(flightRulesObject)
+            print(flightRulesObject)
+            # Send a Message to PubNub with the Metar data
+            flightRulesString = json.dumps(flightRulesObject)
+            #print(urllib.parse.quote(flightRulesString))
+            pubnubURL = 'https://ps.pndsn.com/publish/pn-key/pn-subkey/0/mapPi/myCallback/'+urllib.parse.quote(flightRulesString)+'?store=0&uuid=MatsMapPi'
+
+            with urllib.request.urlopen(pubnubURL) as response:
+                print("Message Sent to PubNub")
             updateLEDsFromMETARs()
 
 
@@ -61,12 +68,17 @@ def updateLEDsFromMETARs():
         setLED(airport, flightRulesObject[airport])
     # Sleep for 55 minutes
     time.sleep(3360) 
+
+
+
+
  
 GPIO.setmode(GPIO.BOARD)
 pwmFrequency = 100.0
 ledPWMMatrix = {}
 
-ledPins = {'KPDK':(19,21,23),'KRYY':(11,13,15),'KLZU':(33,35,37),'KPUJ':(3,5,7),'KATL':(36,38,40)}
+ledPins = {'KATL':(19,21,23),'KJFK':(11,13,15),'KSFO':(33,35,37),'KMIA':(3,5,7),'KMCO':(36,38,40)}
+# ledPins = eval(os.getenv('metarObject'))
 colors = {'RED':(20.0,0.0,0.0),'GREEN':(0.0,50.0,0.0),'BLUE':(0.0,0.0,100.0),'PURPLE':(50.0,0.0,50.0), 'OFF':(0.0,0.0,0.0)}
 
 
@@ -99,8 +111,6 @@ time.sleep(2)
 print('Going Purple')
 allLEDs('PURPLE')
 time.sleep(2)
-
-# I like keeping to the Red,Green,Blue & Extras narative rather than VFR, MVFR, IFR, LIFR. 
 
 # Get All the Metars
 print('Getting Metars')
